25b331d37cfedcc99d4512c0feffccf277fd9361
shcharbakhin <113371130+shcharbakhin@users.noreply.github.com>
2022-09-23 22:24:35 +0300

    Add files via upload
    решение 10 задачи

---
+++ Решение 10 задачи.cs
@@ -0,0 +1,17 @@
+﻿{
+    int A, B, C, D;
+    Console.WriteLine("Введите трехзначное число");
+    A = Convert.ToInt32(Console.ReadLine());
+    B = A % 10;
+    C = (A / 100) * 100;
+    if (100 < A && A < 1000)
+    {
+        D = (A - B - C) / 10;
+        Console.Write("Второе цифра числа: ");
+        Console.WriteLine(D);
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели неправильное число. Запустите программу снова и введите трехзначное число");
+    }
+}
