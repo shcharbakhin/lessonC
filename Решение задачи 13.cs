4adf8edca0ebc16a09cc8a036d06ca67dafe850d
shcharbakhin <113371130+shcharbakhin@users.noreply.github.com>
2022-09-23 22:48:00 +0300

    Add files via upload
    Решение задачи 13

---
+++ Решение задачи 13.cs
@@ -0,0 +1,16 @@
+﻿{
+    int A, B, C;
+    Console.WriteLine("Введите число");
+    A = Convert.ToInt32(Console.ReadLine());
+    C = (A / 1000) * 1000;
+    if (A < 100)
+    {
+        Console.WriteLine("В числе нет третье цифры");
+    }
+    else
+    {
+        B = (A - C) / 100;
+        Console.Write("Третья цифра числа: ");
+        Console.WriteLine(B);
+    }
+}
