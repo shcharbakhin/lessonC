f25fe6ca78f1ba36d0eeb618f31151cb30916029
shcharbakhin <113371130+shcharbakhin@users.noreply.github.com>
2022-09-23 23:02:46 +0300

    Add files via upload
    Решение  задачи 15

---
+++ решение задачи 15.cs
@@ -0,0 +1,23 @@
+﻿{
+    int A;
+    Console.WriteLine("Введите порядковый номер дня недели");
+    A = Convert.ToInt32(Console.ReadLine());
+    if (A > 7)
+    {
+        Console.WriteLine("Вы наверное ошиблись. Такого дня в неделе нет.");
+    }
+    else
+    {
+        if (A < 5)
+        {
+        Console.WriteLine(A);
+        Console.Write("этот день не является выходным");
+        }
+
+        else
+        {
+            Console.WriteLine(A);
+            Console.Write("этот день является выходным");
+        }
+    }
+}
